8d2df805a73b326a1b0473d12341e346a9d408fb
Velondev <philipp.diegner@velon.cc>
2025-07-15 21:55:18 +0200

    Update login.tsx
    General login from supabase

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,10 +1,6 @@
 import { useState } from 'react'
 import { useRouter } from 'next/router'
-import { createClient } from '@supabase/supabase-js'
-
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+import { supabase } from '../utils/supabaseClient'
 )
 
 export default function Login() {
