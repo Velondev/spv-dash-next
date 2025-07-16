051c0696200a0df666068bee7c3377874f81b448
Velondev <philipp.diegner@velon.cc>
2025-07-16 08:57:22 +0200

    Add files via upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,10 @@
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
-import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
-
-export default function IndexPage() {
+export default function Home() {
   const router = useRouter()
-
   useEffect(() => {
-    const checkSession = async () => {
-      const {
-        data: { session }
-      } = await supabase.auth.getSession()
-
-      if (session) {
-        router.push('/dashboard')
-      } else {
-        router.push('/login')
-      }
-    }
-
-    checkSession()
+    router.push('/login')
   }, [router])
-
-  return <div>Lade Speedville Dashboard...</div>
+  return null
 }
