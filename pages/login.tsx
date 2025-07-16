88ca1c3132213949d80910b4c757c8cdf3f42fcb
Velondev <philipp.diegner@velon.cc>
2025-07-16 09:13:19 +0200

    Update login.tsx

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { useRouter } from 'next/router'
 import { supabase } from '../utils/supabaseClient'
-)
+
 
 export default function Login() {
   const router = useRouter()
