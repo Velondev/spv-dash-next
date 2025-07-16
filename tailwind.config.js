41e73d74bade4b78e7b09f6e361f4db1f3045258
Velondev <philipp.diegner@velon.cc>
2025-07-16 23:05:24 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}', // falls du /app-dir nutzt (Next.js 13+)
   ],
   theme: {
     extend: {},
