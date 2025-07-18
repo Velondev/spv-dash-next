158115b0a84af2d0b7a73a9a6aca3bc1036799ff
Velondev <philipp.diegner@velon.cc>
2025-07-18 22:11:53 +0200

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,14 @@
-// pages/_document.tsx
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Font Awesome CDN */}
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
-          integrity="sha512-pGdUJ7DFc4Q+eD+StzDJy5/FtLZo2Z2TvEGeK9bAElPi8yR6vFQOeM8H1KHxkzUw0w4nI6h4Xv6MBSLSu6Mc9g=="
+          integrity="sha512-KKyZOtxAcpVRaZ95IkbSkxRG8u4CR3e1Ncn+uTkhLhFQZfV1bnjqBRSU+Qu53F4NjZaz7o7Z4Gh8GnQobKcVPw=="
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
         />
