48bd627758abb10bdc577653b1fd53451d50c955
Velondev <philipp.diegner@velon.cc>
2025-07-23 23:58:51 +0200

    Update workoutlist.tsx

--- pages/admin/workoutlist.tsx
+++ pages/admin/workoutlist.tsx
@@ -78,7 +78,7 @@ export default function WorkoutList() {
                   weightedPower += d * p
                 }
               }
-            })
+            )}
           }
         } catch (e) {
           console.warn('Parsing-Fehler:', e)
